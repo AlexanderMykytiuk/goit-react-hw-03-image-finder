565dc76ec191f117c5e644565bb3e3af9c90f19b
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2021-03-13 07:45:39 +0200

    Підготував репозиторій до ДЗ 3(2)

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+
+class App extends Component {
+  state = {
+    
+  };
+
+
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem('contacts');
+  //   const persedContacts = JSON.parse(contacts);
+  //   if (persedContacts) {
+  //     this.setState({ contacts: persedContacts });
+  //   }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  render() {
+      return ()
+  }
+}
+export default App;
